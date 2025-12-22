65b642b65ee81533387977ec5fa4c54ec138cc45
M0ck1ngjay178 <margobonal11010@gmail.com>
2025-12-21 22:27:21 -0500

    add entry feild id and init js file

---
+++ ProgramCode/TheVulcanActivityTracker/templates/style/js/login.js
@@ -0,0 +1,3 @@
+//this file will handle login click events, forma verification, and redirection
+//will serve as api for login user account set up
+//possibly hold password hashing functions but prob not

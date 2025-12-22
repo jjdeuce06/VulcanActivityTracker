c7b5af699cbe8d3fa513d0c0e73993e92202641e
M0ck1ngjay178 <margobonal11010@gmail.com>
2025-12-22 16:30:16 -0500

    add login button listener

--- SourceCode/templates/style/js/login.js
+++ SourceCode/templates/style/js/login.js
@@ -1,3 +1,15 @@
 //this file will handle login click events, forma verification, and redirection
 //will serve as api for login user account set up
 //possibly hold password hashing functions but prob not
+
+
+document.getElementById("loginBtn").addEventListener("click", () =>{
+    const username = document.getElementById("user_entry").value;
+    const password = document.getElementById("pass_entry").value;
+
+    console.log("Username:", username);
+    console.log("Password:", password);
+    
+
+
+});

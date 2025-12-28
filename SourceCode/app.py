81e89b323b0e4aeb251b76e856dffe4e3d806e7b
M0ck1ngjay178 <margobonal11010@gmail.com>
2025-12-28 12:48:33 -0500

    init login schema

--- SourceCode/app.py
+++ SourceCode/app.py
@@ -1,18 +1,3 @@
-"""import flask
-from flask import Flask, jsonify, request, Blueprint
-from server.blueprints.blue import blue, style_folder
-from server.api.login_api import login_api
-
-app = Flask(__name__)
-
-app.register_blueprint(blue)
-app.register_blueprint(style_folder)
-app.register_blueprint(login_api, url_prefix="/login_api")
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=6455, debug=True)
-"""
-
 import flask
 from flask import Flask, jsonify, request, Blueprint
 from server.blueprints.blue import blue, style_folder

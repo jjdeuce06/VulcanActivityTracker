a0f9666bce62f6f36786cd9c15dd8551a8a47827
John Gerega <jjgerega@gmail.com>
2026-01-19 12:50:43 -0500

    Design changes, and removal of dashboard_api

--- SourceCode/server/api/dashboard_api.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, jsonify, Blueprint, redirect, url_for
-dashboard_api = Blueprint('dashboard_api', __name__)
-
-@dashboard_api.route('/dashboard_api', methods=['GET'])
-def dashboard():
-    redirect(url_for("blue.dashboard_page"))
-    return jsonify({"message": "Login successful"}), 200
